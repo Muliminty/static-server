bf5207405e147240c3280cd1a4f8191ca570ff24
Muliminty <313195303@qq.com>
2025-01-29 21:41:22 +0800

    feat： home  dev

---
+++ dist/home/assets/GlitchLoader-DDha7O4_.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-main-BbuV3fAc.js";const i=({text:s})=>a.jsx("div",{className:"loader",children:a.jsx("div",{"data-glitch":s,className:"glitch",children:s})});export{i as G};
